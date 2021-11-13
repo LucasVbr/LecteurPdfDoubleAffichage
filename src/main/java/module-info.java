5faa0c5f87b7b6815b98cf4625c6738af6e8a4ba
LucasV-IUT <lucas.vabre@iut-rodez.fr>
2021-11-13 21:54:46 +0100

    Add FichierPDF.java Todo : Terminer les Tests unitaires

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.lecteurpdfdoubleaffichage.lecteurpdfdoubleaffichage {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.apache.pdfbox;
+    requires java.desktop;
 
 
     opens com.lecteurpdfdoubleaffichage.lecteurpdfdoubleaffichage to javafx.fxml;
