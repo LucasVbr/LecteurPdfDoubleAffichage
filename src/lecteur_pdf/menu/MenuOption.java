1ac825612253883380ae4914ea48991588b4f1c5
NonoL3Robot <no.way.l3.robot+github@gmail.com>
2021-11-18 17:01:00 +0100

    Début menu options

---
+++ src/lecteur_pdf/menu/MenuOption.java
@@ -0,0 +1,42 @@
+/*
+ * MenuOption.java, 18/11/2021
+ * IUT Rodez 2021-2021, INFO2
+ * pas de copyright, aucun droits
+ */
+
+package lecteur_pdf.menu;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+/**
+ * TODO class comment
+ * @author Noé VILLENEUVE
+ * @version 1.0
+ */
+public class MenuOption {
+
+    /* Déclaration des éléments visuels */
+    JFrame menuOptions = new JFrame();
+    JLabel label = new JLabel();
+
+    /* Déclaration des listes */
+    ArrayList<String> listeOptions;
+    ArrayList<KeyStroke> listeRaccourcis;
+
+    /**
+     * TODO commenter la méthode
+     */
+    public void menuOptions() {
+        // TODO
+    }
+
+    /**
+     * TODO commenter la méthode
+     * @param key
+     * @param menuItem
+     */
+    public void setMenuItemAccelerator(KeyStroke key, JMenuItem menuItem) {
+        // TODO
+    }
+}
---
+++ src/lecteur_pdf/menu/SelectionnerFichier.java
@@ -0,0 +1,30 @@
+/*
+ * SelectionnerFichier.java, 18/11/2021
+ * IUT Rodez 2021-2021, INFO2
+ * pas de copyright, aucun droits
+ */
+
+package lecteur_pdf.menu;
+
+import javax.swing.*;
+import java.io.File;
+
+/**
+ * TODO class comment
+ * @author Noé VILLENEUVE
+ * @version  1.0
+ */
+public class SelectionnerFichier {
+
+    final String TYPE_FILE = "";
+
+    JFileChooser fileChooser = new JFileChooser();
+
+    /**
+     * TODO commenter la méthode
+     */
+    public File selectionnerFichier() {
+        // TODO
+        return null; // bouchon
+    }
+}
