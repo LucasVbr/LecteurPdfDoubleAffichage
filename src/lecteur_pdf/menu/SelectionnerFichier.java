c14907af2ba4decdc0fafdc5344be9492855c8d3
NonoL3Robot <no.way.l3.robot+github@gmail.com>
2021-11-25 21:42:46 +0100

    ouvrirFichier() opérationnel

--- src/lecteur_pdf/menu/SelectionnerFichier.java
+++ src/lecteur_pdf/menu/SelectionnerFichier.java
@@ -5,15 +5,10 @@
  */
 package lecteur_pdf.menu;
 
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.rendering.PDFRenderer;
-
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.filechooser.FileSystemView;
-import java.awt.event.ActionEvent;
 import java.io.File;
-import java.io.IOException;
 /**
  * TODO class comment
  *
@@ -28,36 +23,27 @@ public class SelectionnerFichier {
     /**
      * Méthode qui créée une fenêtre pour que l'utilisateur choisisse un
      * fichier PDF
-     * @param actionEvent un action event
      * @return file le fichier choisi par l'utilisateur
      */
-    public static File ouvrirFichier(ActionEvent actionEvent) {
-
-        JFrame JFileChooserStage = new JFrame();
+    public static File ouvrirFichier() {
 
         JFileChooser fileChooser = new JFileChooser
                      (FileSystemView.getFileSystemView().getHomeDirectory());
         fileChooser.setDialogTitle("Sélectionnez un PDF");
-//        fileChooser.setMultiSelectionEnabled(true);
+        fileChooser.setMultiSelectionEnabled(false);
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
         fileChooser.setAcceptAllFileFilterUsed(false);
         FileNameExtensionFilter
-            filter = new FileNameExtensionFilter("PDF files", "pdf");
+            filter = new FileNameExtensionFilter("PDF files (*.pdf)", "pdf");
         fileChooser.addChoosableFileFilter(filter);
 
-//        try {
-//            PDDocument document = new PDDocument();
-//            PDFRenderer render = new PDFRenderer(document);
-//            PDDocument.load(file);
-//            return new File(fileChooser.getSelectedFile().getPath());
-//        } catch (Exception e) { // TODO préciser erreur
-//            e.printStackTrace();
-//        }
-
-//        int returnValue = jfc.showOpenDialog(null);
-//        if (returnValue == JFileChooser.APPROVE_OPTION) {
-//            System.out.println(jfc.getSelectedFile().getPath());
-//        }
-
-        return new File(fileChooser.getSelectedFile().getPath());
+        File file;
+        int returnValue = fileChooser.showOpenDialog(null);
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            file = new File(fileChooser.getSelectedFile().getPath());
+        } else {
+          file = null;
+        }
+        return file;
     }
 }
