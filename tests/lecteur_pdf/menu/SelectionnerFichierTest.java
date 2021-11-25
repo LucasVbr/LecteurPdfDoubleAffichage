c0e06ccd3c7f1667bcc88eb17c3a7e943fde50aa
NonoL3Robot <no.way.l3.robot+github@gmail.com>
2021-11-25 21:43:18 +0100

    début des tests de SelectionnerFichier.java

--- tests/lecteur_pdf/menu/SelectionnerFichierTest.java
+++ tests/lecteur_pdf/menu/SelectionnerFichierTest.java
@@ -1,14 +1,19 @@
 package lecteur_pdf.menu;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
 
-import static org.junit.jupiter.api.Assertions.*;
+import lecteur_pdf.menu.Menu;
+
+import javax.swing.*;
 
 class SelectionnerFichierTest {
 
     @Test
     void ouvrirFichier() {
+        Menu menu = new Menu();
+        SelectionnerFichier.ouvrirFichier();
     }
 }
