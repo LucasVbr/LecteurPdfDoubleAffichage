be407c4904b79970746bae7212999ed163136e5e
LucasV-IUT <lucas.vabre@iut-rodez.fr>
2021-11-28 17:58:19 +0100

    Classes PDFTest.java et PageTest.java => Terminées

--- tests/lecteur_pdf/document/PDFTest.java
+++ tests/lecteur_pdf/document/PDFTest.java
@@ -8,25 +8,28 @@ import java.io.IOException;
 class PDFTest {
 
     /**
-     * TODO comment main
+     * Test du constructeur {@link PDF#PDF(File)}
      *
-     * @param args
+     * @param args non utilisé
      */
     public static void main(String[] args) throws IOException {
+        /* Mise en place du JFrame de test */
         JFrame frame = new JFrame();
-
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
-
         frame.setSize(300,300);
         frame.setBackground(Color.gray);
         frame.setVisible(true);
 
+        /* Creation de l’objet PDF */
         PDF doc = new PDF(new File("F:/test_pdf1.pdf"));
 
+        /* Creation du JScrollPane contenant notre PDF pour pouvoir scroller */
         JScrollPane scrollPane = new JScrollPane(doc);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+        /* Ajout du ScrollPane dans la frame et mise à jour de la frame */
         frame.add(scrollPane);
         frame.validate();
     }
