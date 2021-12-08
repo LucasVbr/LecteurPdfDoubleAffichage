8afc5c8c7cdd3536b347b1cef51566ba87ea8e96
NonoL3Robot <no.way.l3.robot+github@gmail.com>
2021-12-08 22:13:10 +0100

    Ajout du .java qui lance l'application

---
+++ src/lecteur_pdf/LecteurPDFDoubleAffichage.java
@@ -0,0 +1,21 @@
+/*
+ * LecteurPDFDoubleAffichage.java, 08/12/2021
+ * IUT Rodez 2021-2021, INFO2
+ * pas de copyright, aucun droits
+ */
+
+package lecteur_pdf;
+
+import lecteur_pdf.affichage.Fenetre;
+
+/**
+ * Classe qui lance le lecteur PDF
+ */
+public class LecteurPDFDoubleAffichage {
+    /**
+     * Crée une nouvelle instance du lecteur PDF
+     *
+     * @param args
+     */
+    public static void main(String[] args) {new Fenetre();}
+}
