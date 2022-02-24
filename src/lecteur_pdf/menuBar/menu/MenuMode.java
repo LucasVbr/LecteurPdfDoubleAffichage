9fd33b57bfc6d108fc744ada98bb46db77a93ba2
LucasV-IUT <lucas.vabre@iut-rodez.fr>
2022-02-24 14:10:43 +0100

    Update MenuMode.java

--- src/lecteur_pdf/menuBar/menu/MenuMode.java
+++ src/lecteur_pdf/menuBar/menu/MenuMode.java
@@ -11,23 +11,26 @@ import javax.swing.*;
 public class MenuMode extends JMenu {
     public MenuMode(IhmPdf parent) {
         super("Mode");
-        // Ajoute les menu items
-        add(new NouvelleFenetre(parent));
 
-        ButtonGroup groupe = new ButtonGroup();
         ModeSepare modeSepare = new ModeSepare(parent);
         ModeSynchronise modeSynchronise = new ModeSynchronise(parent);
 
-        add(modeSepare);
-        add(modeSynchronise);
-
+        /* Crée un groupe de Radio */
+        ButtonGroup groupe = new ButtonGroup();
         groupe.add(modeSepare);
         groupe.add(modeSynchronise);
 
-
+        /* Initialise les boutons selectionnés */
         modeSepare.setSelected(!GestionPdf.modeSynchronise);
         modeSynchronise.setSelected(GestionPdf.modeSynchronise);
 
         parent.setMode(modeSepare, modeSynchronise);
+
+        /* Ajoute les menu items */
+        add(new NouvelleFenetre(parent));
+        addSeparator();
+        add(modeSepare);
+        add(modeSynchronise);
+
     }
 }
